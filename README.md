94fb9b966db81679ceb6398f7cecfa694d40abee
alisa985 <2960786950@qq.com>
2021-01-22 17:16:12 +0800

    基本布局已完成

---
+++ README.md
@@ -0,0 +1,19 @@
+# myBlog
+## nodejs+mongodb简单的博客
+### aaa
+
+
+
+
+
+
+1、使用mongoose连接数据库
+2、配置公共sceema规则模块
+3、schema模板
+4、注册
+5、登录
+6、首页文章查询
+7、分页删除
+8、
+9、
+10、
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+//登录路由配置
+router.get('/login',function(req,res){
+  res.render('login',{})
+})
+//文章详情页路由配置
+router.get('/xiangqing',function(req,res){
+  res.render('xiangqing',{})
+})
+//注册页路由配置
+router.get('/zhuce',function(req,res){
+  res.render('zhuce',{})
+})
+//wirte页路由配置
+router.get('/write',function(req,res){
+  res.render('write',{})
+})
+//分页路由配置
+router.get('/fenye',function(req,res){
+  res.render('fenye',{})
+})
 
 module.exports = router;
